06e2b360898ada19640ceefb535fe850cb10068f
DeJesus <VelezR@ad1.prod>
2018-05-03 10:44:49 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,12 @@
-console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var CardFour = "king";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [0, 1, 2, 3];
+var cardOne = cards[0];
+var cardTwo = cards[1];
+cardsInPlay.push("queen", "king");
+console.log("User flipped queen");
+console.log("User flipped king");
+if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1] || cardsInPlay[2] === cardsInPlay[3]) {
+    alert("You found a match!");
+} else if (cardsInPlay[1] !== cardsInPlay[2]) {
+    alert("Sorry, try again");
+}
