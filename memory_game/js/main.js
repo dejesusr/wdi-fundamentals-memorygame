3020cfd3205e8859427c876685306a56339363a8
DeJesus <VelezR@ad1.prod>
2018-05-03 18:23:03 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,12 +1,15 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [0, 1, 2, 3];
-var cardOne = cards[0];
-var cardTwo = cards[1];
-cardsInPlay.push("queen", "king");
-console.log("User flipped queen");
-console.log("User flipped king");
-if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1] || cardsInPlay[2] === cardsInPlay[3]) {
-    alert("You found a match!");
-} else if (cardsInPlay[1] !== cardsInPlay[2]) {
-    alert("Sorry, try again");
+cardsInPlay.push("king");
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+        console.log("You found a match!");
+    } else {
+        console.log("Sorry, try again");
+    }
+var flipCard = function (cardId) {
+    console.log("User flipped " + cards[cardId]);
 }
+
+flipCard(0);
+flipCard(2);
+
